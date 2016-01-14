909772f12409a6021c446b53e7f928adf936e190
Jake Steinberg and Sean Peerenboom <student@epicodus.com>
2016-01-14 15:57:10 -0800

    Add all existing files to github.

---
+++ js/scripts.js
@@ -0,0 +1,84 @@
+$(document).ready(function() {
+alert("The best music is Schwifty");
+$("form#formid1").submit(function(event) {
+
+var age = parseInt($("input#age").val());
+var musical =$("input#musical").val();
+var alien = $("select#alien").val();
+
+if (alien === "Gearhead" && alien !== "Birdperson" && alien !== "Zeke" && age>60 && musical !=='Schwifty') {
+  $('#rick').show();
+} else {
+  $('#rick').hide();
+}
+
+if (alien !== "Gearhead" && alien === "Birdperson" && alien !== "Zeke" && age>60 && musical !=='Schwifty') {
+  $('#morty').show();
+} else {
+  $('#morty').hide();
+}
+
+if (alien !== "Gearhead" && alien !== "Birdperson" && alien === "Zeke" && age>60 && musical !=='Schwifty') {
+  $('#jerry').show();
+} else {
+  $('#jerry').hide();
+}
+if (age< 60 && age >= 40 && (musical !=='Schwifty')) {
+  $('#beth').show();
+} else {
+  $('#beth').hide();
+}
+if (age>1 && age<=18 && (musical !=='Schwifty')) {
+  $('#summer').show();
+} else {
+  $('#summer').hide();
+}
+if (age>0 && musical ==='Schwifty') {
+  $('#meseeks').show();
+} else {
+  $('#meseeks').hide();
+}
+event.preventDefault();
+});
+});
+
+
+
+//
+// if (alien === "Birdperson") {
+//   $('#morty').show();
+// } else {
+//   $('#morty').hide();
+// }
+//
+// if (alien === "Zeke") {
+//   $('#summer').show();
+// }  else {
+//     $('#summer').hide();
+// }
+//
+// if (age>=1 && age<10) {
+//   $('#meseeks').show();
+//   $('#morty').hide();
+//   $('#summer').hide();
+//   $('#rick').hide();
+// } else {
+//     $('#meseeks').hide();
+// }
+//
+// if (age>=50 && age<100){
+//   $('#jerry').show();
+//   $('#morty').hide();
+//   $('#summer').hide();
+//   $('#rick').hide();
+//  else {
+//   $('#jerry').hide();
+//  }
+//  if (musical ==='Schwifty') {
+//    $('#beth').show();
+//    $('#morty').hide();
+//    $('#summer').hide();
+//    $('#rick').hide();
+//    else {
+//     $('#beth').hide();
+// }
